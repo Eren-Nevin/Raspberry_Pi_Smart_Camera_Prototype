cefaa168d18f42f1887c25205ed3664e1d27db28
mvp <maverickvparker@gmail.com>
2022-04-19 16:28:48 -0900

    Fourth Commit

--- simple_cam.py
+++ simple_cam.py
@@ -1,7 +1,7 @@
 from picamera import PiCamera
 from time import sleep
 
-picamera.start_preview()
+camera = PiCamera()
+camera.start_preview()
 sleep(5)
-picamera.capture('hello.jpg')
 camera.stop_preview()
