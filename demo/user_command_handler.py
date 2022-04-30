96a51fae5793baab363c30d550c447ef45ab9330
MVP <maverickvparker@gmail.com>
2022-04-30 17:39:58 +0300

    Refactored webcam_face_detect to create face_detector object and also provide two distinct cameras one using cv2 working on all platforms and another using picamera only for raspberry pi

--- demo/user_command_handler.py
+++ demo/user_command_handler.py
@@ -1,7 +1,8 @@
+from io import BytesIO
 from pathlib import PosixPath
 from pyrogram import Client, filters
 from pyrogram.types.messages_and_media.message import Message
-from webcam_face_detect import VideoFaceDetector, Face, KnownFace, read_known_faces_from_directory
+from webcam_face_detect import RPiFaceDetector, GeneralFaceDetectorCamera, Face, KnownFace, read_known_faces_from_directory
 import cv2
 import asyncio
 
@@ -23,18 +24,15 @@ from time import time
 
 # found_one = False
 
-async def start(client: Client, message: Message):
+async def handle_faces_general(client: Client, message: Message):
     known_faces = read_known_faces_from_directory('./known_faces')
-    video_face_detector = VideoFaceDetector()
+    video_face_detector = GeneralFaceDetectorCamera()
     while True:
         frame, rgb_small_frame = video_face_detector.capture_frame()
-        t1 = time()
         found_face_locations, found_face_encodings =\
             video_face_detector.detect_faces(rgb_small_frame)
-        t2 = time()
         found_faces = video_face_detector.recognize_faces(known_faces,
                                                           found_face_encodings)
-        t3 = time()
         for face in found_faces:
             # await client.send_message(message.chat.id, f"{face.name} Seen")
             # frame_photo = cv2.imencode('.jpg', frame)[1]
@@ -43,10 +41,30 @@ async def start(client: Client, message: Message):
             await client.send_photo(message.chat.id, "pic.jpg",
                                     caption=face.name)
 
-        t4 = time()
-        print(f"{t2-t1} for detection, {t3 - t2} for recognition, {t4 - t3} for\
-              sending")
         await asyncio.sleep(3)
+
+async def handle_faces_rpi(client: Client, message: Message):
+    known_faces = read_known_faces_from_directory('./known_faces')
+    video_face_detector = RPiFaceDetector("320x240")
+    while True:
+        rgb_small_frame = video_face_detector.capture_frame()
+        bytes_io = BytesIO(rgb_small_frame.tobytes())
+        found_face_locations, found_face_encodings =\
+            video_face_detector.detect_faces(rgb_small_frame)
+        found_faces = video_face_detector.recognize_faces(known_faces,
+                                                          found_face_encodings)
+        for face in found_faces:
+            # await client.send_message(message.chat.id, f"{face.name} Seen")
+            # frame_photo = cv2.imencode('.jpg', frame)[1]
+            face.name = face.name if face.isKnown else "Unknown"
+            # cv2.imwrite(f"pic.jpg", frame)
+            await client.send_photo(message.chat.id, bytes_io,
+                                    caption=face.name)
+
+        await asyncio.sleep(3)
+
+async def start(client: Client, message: Message):
+    await handle_faces_general(client, message)
 
 
 
